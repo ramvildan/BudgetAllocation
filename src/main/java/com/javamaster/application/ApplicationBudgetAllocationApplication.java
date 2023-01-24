b7e217ecd617a680650e67444d86c2b6bfe35e86
Ramil Vildanov <ramvildan.work@gmail.com>
2023-01-24 16:08:30 +0300

    Initial commit

---
+++ src/main/java/com/javamaster/application/ApplicationBudgetAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.javamaster.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBudgetAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBudgetAllocationApplication.class, args);
+	}
+
+}
