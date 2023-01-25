6070ad4dc83698a3a8b96bd6e228560521b3f752
Ramil Vildanov <ramvildan.work@gmail.com>
2023-01-25 16:36:26 +0300

    ValidationException.java

---
+++ src/main/java/ValidationException.java
@@ -0,0 +1,11 @@
+public class ValidationException extends Exception {
+
+    private String message;
+
+    public ValidationException(String message) {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
