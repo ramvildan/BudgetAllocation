1141297cbf18446dfaea733316e86a0c7e7586ca
Ramil Vildanov <ramvildan.work@gmail.com>
2023-01-25 16:37:37 +0300

    Added converter from User to UserDto and from UserDto to User

---
+++ src/main/java/UsersConverter.java
@@ -0,0 +1,23 @@
+import dto.UsersDto;
+import repository.Users;
+
+public class UsersConverter {
+
+    public Users fromUserDtoToUser(UsersDto usersDto) {
+        Users users = new Users();
+        users.setId(usersDto.getId());
+        users.setEmail(usersDto.getEmail());
+        users.setName(usersDto.getName());
+        users.setLogin(usersDto.getLogin());
+        return users;
+    }
+
+    public UsersDto fromUserToUserDto(Users users) {
+        return UsersDto.builder()
+                .id(users.getId())
+                .email(users.getEmail())
+                .login(users.getLogin())
+                .name(users.getName())
+                .build();
+    }
+}
