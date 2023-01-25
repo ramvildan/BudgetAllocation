d91b4d9df37f4b83c447d6140e49a5a1b9367a24
Ramil Vildanov <ramvildan.work@gmail.com>
2023-01-25 16:33:21 +0300

    Added class Users.java and UsersDto.java

---
+++ src/main/java/repository/Users.java
@@ -0,0 +1,26 @@
+package repository;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users_table")
+@Data
+@NoArgsConstructor
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String login;
+
+    @Column
+    private String email;
+
+}
